9fa0a015a04be65fc1a577d3def47392e58e18ea
Tulip <12496016+LunarTulip@users.noreply.github.com>
2018-11-03 14:17:24 -0700

    Started work on client

--- client.cc
+++ client.cc
@@ -1,3 +1,27 @@
 // Alyssa Riceman and Monica Moniot
 
-// Cache client implementation goes here
+#include <iostream>
+#include <cassert>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "cache.h"
+
+uint16_t PORTNO = 8765;
+const char* ipAddress = "127.0.0.1";
+
+int main() {
+    int clientSocket;
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    assert(clientSocket >= 0 && "Socket creation failed.");
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = PORTNO;
+    inet_pton(AF_INET, ipAddress, &serverAddress.sin_addr.s_addr);
+
+    const sockaddr *addressPointer = reinterpret_cast<const sockaddr*>(&serverAddress);
+    int connectionSuccess = connect(clientSocket, addressPointer, sizeof(serverAddress));
+    std::cout << connectionSuccess << "\n";
+    assert(connectionSuccess == 0 && "Connection failed.");
+}
