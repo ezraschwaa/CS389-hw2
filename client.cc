f5e169350d413ad06b9a5cb3e88fdaa0e33931f6
Tulip <12496016+LunarTulip@users.noreply.github.com>
2018-11-11 10:47:45 -0800

    Fixed socket handling

--- client.cc
+++ client.cc
@@ -13,8 +13,8 @@
 
 typedef int socketType;
 struct cache_obj {
-    socketType tcpSocket;
-    socketType udpSocket;
+    uint16_t portNum;
+    const char* ipAddress;
 };
 
 uint16_t PORTNUM = 33052;
@@ -80,9 +80,45 @@ char* getStatusCode(char* sourceBuffer, int sourceBufferLength) {
     return statusCodeStr;
 }
 
-// int handleStatusCode(char* sourceBuffer, int sourceBufferLength) {
-//     char* statusCode;
+int handleStatusCode(char* sourceBuffer, int sourceBufferLength) {
+    char* statusCode;
 
+    int success = 0;
+    bool codeNeeded = true;
+    while (codeNeeded) { // Loop waiting for status code
+        statusCode = getStatusCode(sourceBuffer, sourceBufferLength);
+        switch(statusCode[0]) {
+            case '1':
+                delete[] statusCode;
+                continue;
+            case '2':
+                codeNeeded = false;
+            case '3':
+                delete[] statusCode;
+                continue;
+            case '4':
+                success = -1;
+                codeNeeded = false;
+            case '5':
+                success = -1;
+                codeNeeded = false;
+        }
+    }
+
+    delete[] statusCode;
+    return success;
+}
+
+// char* readMessage(char* sourceBuffer, int sourceBufferLength) {
+//     int messageStart = 0;
+//     bool hitOneNewline = false;
+//     while (messageStart < sourceBufferLength) {
+//         if (sourceBuffer[messageStart] == '\n' &&){
+//             statusCodeStart++;
+//             break;
+//         }
+//         statusCodeStart++;
+//     }
 // }
 
 socketType start_socket(int commType, uint16_t portNum, const char* ipAddress) {
@@ -103,90 +139,55 @@ socketType start_socket(int commType, uint16_t portNum, const char* ipAddress) {
 }
 
 cache_type create_cache(index_type maxmem, hash_func hasher) {
-    socketType tcpSocket = start_socket(SOCK_STREAM, PORTNUM, LOCALHOST);
-    socketType udpSocket = start_socket(SOCK_DGRAM, PORTNUM, LOCALHOST);
+    // socketType tcpSocket = start_socket(SOCK_STREAM, PORTNUM, LOCALHOST);
+    // socketType udpSocket = start_socket(SOCK_DGRAM, PORTNUM, LOCALHOST);
 
-    char* memsizeCstr = reinterpret_cast<char*>(&maxmem);
-
-    // Ignore memsize and hasher; the server API doesn't allow them to work any more
+    // Ignore maxmem and hasher; the server API doesn't allow them to work any more
 
     cache_obj* cacheStruct = new cache_obj;
-    cacheStruct->tcpSocket = tcpSocket;
-    cacheStruct->udpSocket = udpSocket;
+    // cacheStruct->tcpSocket = tcpSocket;
+    // cacheStruct->udpSocket = udpSocket;
+    cacheStruct->portNum = PORTNUM;
+    cacheStruct->ipAddress = LOCALHOST;
 
     return cacheStruct;
 }
 
 int cache_set(cache_type cache, key_type key, val_type val, index_type val_size) {
-    char* setRequest = makeHttpRequest("PUT", "key", key, static_cast<const char*>(val));
+    socketType setSocket = start_socket(SOCK_STREAM, cache->portNum, cache->ipAddress);
+
+    char *setRequest = makeHttpRequest("PUT", "key", key, static_cast<const char *>(val));
     std::cout << setRequest;
-    send(cache->tcpSocket, setRequest, strlen(setRequest), 0);
+    send(setSocket, setRequest, strlen(setRequest), 0);
 
     char* serverReadBuffer = new char[2048];
-    int serverReadLength = read(cache->tcpSocket, serverReadBuffer, 2048);
+    int serverReadLength = read(setSocket, serverReadBuffer, 2048);
     assert(serverReadLength >= 0 && "Failed to read from server.");
     char* statusCode;
 
-    int setSuccess = 0;
-    bool codeNeeded = true;
-    while (codeNeeded) { // Loop waiting for status code
-        statusCode = getStatusCode(serverReadBuffer, 2048);
-        std::cout << statusCode << "\n";
-        switch(statusCode[0]) {
-            case '1':
-                delete[] statusCode;
-                continue;
-            case '2':
-                codeNeeded = false;
-            case '3':
-                delete[] statusCode;
-                continue;
-            case '4':
-                setSuccess = -1;
-                codeNeeded = false;
-            case '5':
-                setSuccess = -1;
-                codeNeeded = false;
-        }
-    }
+    int setSuccess = handleStatusCode(serverReadBuffer, 2048);
 
     delete[] setRequest;
     delete[] serverReadBuffer;
-    delete[] statusCode;
-    return setSuccess; //Placeholder
+    return setSuccess;
 }
 
 val_type cache_get(cache_type cache, key_type key, index_type *val_size) {
-    char* getRequest = makeHttpRequest("GET", "key", key, NULL);
+    socketType getSocket = start_socket(SOCK_STREAM, cache->portNum, cache->ipAddress);
+
+    char *getRequest = makeHttpRequest("GET", "key", key, NULL);
     std::cout << getRequest;
-    send(cache->tcpSocket, getRequest, strlen(getRequest), 0);
+    int sendSuccess = send(getSocket, getRequest, strlen(getRequest), 0);
+    assert(sendSuccess >= 0 && "Failed to send space used request.");
 
     char* serverReadBuffer = new char[2048];
-    int serverReadLength = read(cache->tcpSocket, serverReadBuffer, 2048);
+    int serverReadLength = read(getSocket, serverReadBuffer, 2048);
     assert(serverReadLength >= 0 && "Failed to read from server.");
     char* statusCode = getStatusCode(serverReadBuffer, 2048);
 
-    int setSuccess = 0;
-    bool codeNeeded = true;
-    while (codeNeeded) { // Loop waiting for status code
-        statusCode = getStatusCode(serverReadBuffer, 2048);
-        switch(statusCode[0]) {
-            case '1':
-                delete[] statusCode;
-                continue;
-            case '2':
-                codeNeeded = false;
-            case '3':
-                delete[] statusCode;
-                continue;
-            case '4':
-                setSuccess = -1;
-                codeNeeded = false;
-            case '5':
-                setSuccess = -1;
-                codeNeeded = false;
-        }
-    }
+    int getSuccess = handleStatusCode(serverReadBuffer, 2048);
+
+    //If applicable, read and return reply
 
     delete[] getRequest;
     delete[] statusCode;
@@ -194,46 +195,33 @@ val_type cache_get(cache_type cache, key_type key, index_type *val_size) {
 }
 
 int cache_delete(cache_type cache, key_type key) {
-    char* deleteRequest = makeHttpRequest("DELETE", "key", key, NULL);
+    socketType deleteSocket = start_socket(SOCK_STREAM, cache->portNum, cache->ipAddress);
+
+    char *deleteRequest = makeHttpRequest("DELETE", "key", key, NULL);
     std::cout << deleteRequest;
-    send(cache->tcpSocket, deleteRequest, strlen(deleteRequest), 0);
+    int sendSuccess = send(deleteSocket, deleteRequest, strlen(deleteRequest), 0);
+    assert(sendSuccess >= 0 && "Failed to send delete request.");
 
     char* serverReadBuffer = new char[2048];
-    int serverReadLength = read(cache->tcpSocket, serverReadBuffer, 2048);
+    int serverReadLength = read(deleteSocket, serverReadBuffer, 2048);
     assert(serverReadLength >= 0 && "Failed to read from server.");
     char* statusCode = getStatusCode(serverReadBuffer, 2048);
 
-    int deleteSuccess = 0;
-    bool codeNeeded = true;
-    while (codeNeeded) { // Loop waiting for status code
-        statusCode = getStatusCode(serverReadBuffer, 2048);
-        switch(statusCode[0]) {
-            case '1':
-                delete[] statusCode;
-                continue;
-            case '2':
-                codeNeeded = false;
-            case '3':
-                delete[] statusCode;
-                continue;
-            case '4':
-                deleteSuccess = -1;
-                codeNeeded = false;
-            case '5':
-                deleteSuccess = -1;
-                codeNeeded = false;
-        }
-    }
+    int deleteSuccess = handleStatusCode(serverReadBuffer, 2048);
 
     delete[] deleteRequest;
     delete[] statusCode;
-    return deleteSuccess; //Placeholder
+    return deleteSuccess;
 }
 
 index_type cache_space_used(cache_type cache){
+    socketType spaceUsedSocket = start_socket(SOCK_STREAM, cache->portNum, cache->ipAddress);
+
     char* spaceUsedRequest = makeHttpRequest("GET", "memsize", NULL, NULL);
     std::cout << spaceUsedRequest;
-    send(cache->tcpSocket, spaceUsedRequest, strlen(spaceUsedRequest), 0);
+    int sendSuccess = send(spaceUsedSocket, spaceUsedRequest, strlen(spaceUsedRequest), 0);
+    std::cout << sendSuccess << "\n";
+    assert(sendSuccess >= 0 && "Failed to send space used request.");
 
     // Receive reply from server
 
@@ -242,39 +230,14 @@ index_type cache_space_used(cache_type cache){
 }
 
 void destroy_cache(cache_type cache) {
+    socketType destroySocket = start_socket(SOCK_STREAM, cache->portNum, cache->ipAddress);
+
     char* destroyRequest = makeHttpRequest("POST", "shutdown", NULL, NULL);
     std::cout << destroyRequest;
-    send(cache->tcpSocket, destroyRequest, strlen(destroyRequest), 0);
-
-    char* serverReadBuffer = new char[2048];
-    int serverReadLength = read(cache->tcpSocket, serverReadBuffer, 2048);
-    assert(serverReadLength >= 0 && "Failed to read from server.");
-    char* statusCode = getStatusCode(serverReadBuffer, 2048);
-
-    int setSuccess = 0;
-    bool codeNeeded = true;
-    while (codeNeeded) { // Loop waiting for status code
-        statusCode = getStatusCode(serverReadBuffer, 2048);
-        switch(statusCode[0]) {
-            case '1':
-                delete[] statusCode;
-                continue;
-            case '2':
-                codeNeeded = false;
-            case '3':
-                delete[] statusCode;
-                continue;
-            case '4':
-                setSuccess = -1;
-                codeNeeded = false;
-            case '5':
-                setSuccess = -1;
-                codeNeeded = false;
-        }
-    }
+    int sendSuccess = send(destroySocket, destroyRequest, strlen(destroyRequest), 0);
+    assert(sendSuccess >= 0 && "Failed to send destroy request.");
 
     delete[] destroyRequest;
-    delete[] statusCode;
 }
 
 int main() {
@@ -284,9 +247,9 @@ int main() {
     void* placeholderValue = reinterpret_cast<void*>(valueText);
     index_type placeholderValSize = 15;
     cache_set(testCache, shortCstring, placeholderValue, placeholderValSize);
-    // cache_get(testCache, shortCstring, &placeholderValSize);
-    // cache_delete(testCache, shortCstring);
-    // cache_space_used(testCache);
-    // destroy_cache(testCache);
+    cache_get(testCache, shortCstring, &placeholderValSize); //Weird segfault here; debug when possible
+    cache_space_used(testCache);
+    cache_delete(testCache, shortCstring);
+    destroy_cache(testCache);
     return 1; // Main is for testing only, will vanish from final program
 }
