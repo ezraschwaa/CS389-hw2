e4e2c12a2458bae1f0c3e28a428a46db0305623c
betyoucantguessmyusername <36983270+betyoucantguessmyusername@users.noreply.github.com>
2018-12-01 20:12:35 -0800

    made some edits or something idk

--- performance_eval.cc
+++ performance_eval.cc
@@ -42,7 +42,7 @@ const key_type UNUSEDKEY = "bb";
 const char* SMALLVAL = make_str_of_defined_length(2); //Vals can't be const because they need to be cast to void*
 const index_type SMALLVAL_SIZE = 1; //Val sizes can't be const because cache_get requires a non-const val pointer; size is 1 below size of str length because null terminators aren't part of the val
 const char* LARGEVAL = make_str_of_defined_length(128);
-const index_type LARGEVAL_SIZE = 127;
+const index_type LARGEVAL_SIZE = 30;
 index_type valsize1_for_get = 0;
 index_type valsize2_for_get = 0;
 
@@ -53,7 +53,7 @@ int TRIALS = 1000000;
 ////////////////////
 
 // Test to ensure that cache creation and destruction work with different cache sizes and presence or absence of user-input hash functions, to ensure that any errors which may arise from doing so arise
-int test_create_cache_and_destroy_cache() {
+int test_create_cache() {
     index_type sizeArray[] = {SMALL_CACHE_SIZE, CACHE_SIZE, LARGE_CACHE_SIZE};
 
     for(int i = 0; i < 3; i++) {
@@ -79,21 +79,26 @@ int test_cache_set(cache_type cache) {
     const char* values[] = {SMALLVAL, LARGEVAL};
     index_type valSize[] = {SMALLVAL_SIZE, LARGEVAL_SIZE};
 
-    for(int i = 0; i < 2; i++) {
-        int count = 0;
-        float timer = 0;
-        while (count < TRIALS){
-            auto start = high_resolution_clock::now();
-            cache_set(cache, KEY1, values[i], valSize[i]);
-            auto stop = high_resolution_clock::now();
-            auto duration = duration_cast<nanoseconds>(stop - start);
+    index_type sizeArray[] = {SMALL_CACHE_SIZE, CACHE_SIZE, LARGE_CACHE_SIZE};
+    cache_type cacheArray[] = {create_cache(SMALL_CACHE_SIZE, NULL), create_cache(CACHE_SIZE, NULL), create_cache(LARGE_CACHE_SIZE, NULL)};
 
-            timer += duration.count();
-            ++count;
-            cache_delete(cache, KEY1);
+    for(int i = 0; i < 2; i++) {
+        for(int j=0;j< 3;j++) {
+            int count = 0;
+            float timer = 0;
+            while (count < TRIALS){
+                auto start = high_resolution_clock::now();
+                cache_set(cacheArray[j], KEY1, values[i], valSize[i]);
+                auto stop = high_resolution_clock::now();
+                auto duration = duration_cast<nanoseconds>(stop - start);
+
+                timer += duration.count();
+                ++count;
+                cache_delete(cacheArray[j], KEY1);
+            }
+            timer = (timer / TRIALS);
+            cout << "\tAVG Time to set "<< valSize[i] << " sized val in " << sizeArray[j] << " sized cache: " << timer << " nanoseconds" << endl;
         }
-        timer = (timer / TRIALS);
-        cout << "\tAverage time to set a val of size "<< valSize[i] << ": " << timer << " nanoseconds" << endl;
     }
     return 0;
 }
@@ -101,21 +106,26 @@ int test_cache_delete(cache_type cache) {
     const char* values[] = {SMALLVAL, LARGEVAL};
     index_type valSize[] = {SMALLVAL_SIZE, LARGEVAL_SIZE};
 
-    for(int i = 0; i < 2; i++) {
-        int count = 0;
-        float timer = 0;
-        while (count < TRIALS){
-            cache_set(cache, KEY1, values[i], valSize[i]);
-            auto start = high_resolution_clock::now();
-            cache_delete(cache, KEY1);
-            auto stop = high_resolution_clock::now();
-            auto duration = duration_cast<nanoseconds>(stop - start);
+    index_type sizeArray[] = {SMALL_CACHE_SIZE, CACHE_SIZE, LARGE_CACHE_SIZE};
+    cache_type cacheArray[] = {create_cache(SMALL_CACHE_SIZE, NULL), create_cache(CACHE_SIZE, NULL), create_cache(LARGE_CACHE_SIZE, NULL)};
 
-            timer += duration.count();
-            ++count;
+    for(int i = 0; i < 2; i++) {
+        for(int j=0; j<3; j++) {
+            int count = 0;
+            float timer = 0;
+            while (count < TRIALS){
+                cache_set(cacheArray[j], KEY1, values[i], valSize[i]);
+                auto start = high_resolution_clock::now();
+                cache_delete(cacheArray[j], KEY1);
+                auto stop = high_resolution_clock::now();
+                auto duration = duration_cast<nanoseconds>(stop - start);
+
+                timer += duration.count();
+                ++count;
+            }
+            timer = (timer / TRIALS);
+            cout << "\tAVG Time to delete "<< valSize[i] << " sized val in " << sizeArray[j] << " sized cache: " << timer << " nanoseconds" << endl;        
         }
-        timer = (timer / TRIALS);
-        cout << "\tAverage time to delete val of size "<< valSize[i] << ": " << timer << " nanoseconds" << endl;
     }
     int count = 0;
     float timer = 0;
@@ -125,7 +135,7 @@ int test_cache_delete(cache_type cache) {
 int test_cache_get() {
     index_type sizeArray[] = {SMALL_CACHE_SIZE, CACHE_SIZE, LARGE_CACHE_SIZE};
     cache_type cacheArray[] = {create_cache(SMALL_CACHE_SIZE, NULL), create_cache(CACHE_SIZE, NULL), create_cache(LARGE_CACHE_SIZE, NULL)};
-    
+
     for(int i = 0; i < 3; i++) {
         int count = 0;
         float timer = 0;
@@ -181,7 +191,7 @@ int test_cache_get() {
             ++count;
         }
         timer = (timer / TRIALS);
-        cout << "\tAverage time for space used of filled "<< sizeArray[i] << " cache: " << timer << " nanoseconds" << endl;
+        cout << "\tAverage time for get of filled "<< sizeArray[i] << " cache: " << timer << " nanoseconds" << endl;
     }
 
     for(int j = 0; j < 2; j++){
@@ -193,24 +203,29 @@ int test_cache_get() {
         }
     }
 
-    for(int i = 0; i < 2; i++) {
-        int count = 0;
-        float timer = 0;
-        while (count < TRIALS){
-            string randk = to_string(rand() % sizeArray[i]);
-            key_type randKEY = randk.c_str();
+    // for(int i = 0; i < 2; i++) {
+    //     int count = 0;
+    //     float timer = 0;
+    //     string randk;
+    //     key_type randKEY;
+    //     cout << "Currently on Cache size " << sizeArray[i] << '\n';
+    //     while (count < TRIALS){
+    //         randk = to_string(rand() % sizeArray[i]);
+    //         randKEY = randk.c_str();
+    //         cout << count << "\n";
+    //         auto start = high_resolution_clock::now();
+    //         cache_get(cacheArray[i], randKEY, &valsize1_for_get);
 
-            auto start = high_resolution_clock::now();
-            cache_get(cacheArray[i], randKEY, &valsize1_for_get);
-            auto stop = high_resolution_clock::now();
-            auto duration = duration_cast<nanoseconds>(stop - start);
+    //         auto stop = high_resolution_clock::now();
+    //         auto duration = duration_cast<nanoseconds>(stop - start);
 
-            timer += duration.count();
-            ++count;
-        }
-        timer = (timer / TRIALS);
-        cout << "\tAverage time for space used of filled "<< sizeArray[i] << " cache: " << timer << " nanoseconds" << endl;
-    }
+    //         timer += duration.count();
+    //         ++count;
+    //     }
+    //     timer = (timer / TRIALS);
+    //     cout << "\tAverage time for get of discrete filled "<< sizeArray[i] << " cache: " << timer << " nanoseconds" << endl;
+    //     destroy_cache(cacheArray[i]);
+    // }
     return 0;
 }
 
@@ -280,6 +295,12 @@ int test_cache_space_used(){
 
     return 0;
 }
+
+
+
+
+
+
 int 
 main()
 {
@@ -287,7 +308,7 @@ main()
 
     //Times the tests itself.  Not great for measuring the actual metrics. 
 	cout << "Testing creating cache: " << endl;
-    test_create_cache_and_destroy_cache();
+    test_create_cache();
 
 	cout << "Testing set: " << endl;
 	test_cache_set(cache1);
